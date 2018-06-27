5d8a638139e9e318757340c475553ad4454fe598
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-27 17:48:35 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,13 +1,15 @@
 package com.kole;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<Album> albums = new ArrayList<>();
+        LinkedList<Song> playList = new LinkedList<>();
 
         Album album = new Album("Kill em all", "Metallica");
-        album.a
+        albums.get(0).addSongToPlayList("Kobasica", playList);
     }
 }
